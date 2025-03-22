a7437f3550a1ef0bea1409324dcbd89ef4902dd3
akash-120216 <akash16.04.2005@gmail.com>
2025-03-22 13:55:22 +0530

    Fix line ending warnings

---
+++ GatePass/src/main/java/Srec/example/GatePass/GatePassApplication.java
@@ -0,0 +1,13 @@
+package Srec.example.GatePass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatePassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatePassApplication.class, args);
+	}
+
+}
