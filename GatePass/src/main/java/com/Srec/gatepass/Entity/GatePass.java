603d8f503266fd8f5445e84b146fad48690c1f7f
akash-120216 <akash16.04.2005@gmail.com>
2025-03-23 16:27:18 +0530

    update

--- GatePass/src/main/java/com/Srec/gatepass/Entity/GatePass.java
+++ GatePass/src/main/java/com/Srec/gatepass/Entity/GatePass.java
@@ -3,13 +3,12 @@ package com.Srec.gatepass.Entity;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Getter;
-import lombok.Setter;
 
-@Setter
 @Getter
 @Entity
 @Data
 public class GatePass {
+    // Getters and Setters
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -33,5 +32,6 @@ public class GatePass {
     @ManyToOne
     @JoinColumn(name = "security_id")
     private Security security;
+
 
 }
